42aa03cbd0da322f1621959afb3ac87070d59212
Luke Smith <luke@smith.ac>
2020-04-29 19:53:40 +0100

    Generalise the traffic profile plot code. Add Hull.

--- notebooks/traffic.py
+++ notebooks/traffic.py
@@ -11,6 +11,7 @@ import warnings
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib.patheffects as pe
+import matplotlib.ticker as mtick
 import math
 
 warnings.filterwarnings('ignore')
@@ -26,6 +27,8 @@ dateToday = datetime.datetime.combine(datetime.date.today(), datetime.datetime.m
 dateBaselineEnd = datetime.datetime.strptime('2020-03-15T23:59:59Z', '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=tzLocal)
 plottableTypes = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
 govChartStart = datetime.datetime.strptime('2020-03-01T00:00:00Z', '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=tzLocal)
+lastWeekStart = dateToday - pd.Timedelta(days=6)
+lastWeekEnd = dateToday + pd.Timedelta(hours=24)
 
 bankHolidays = [
     '10-04-2020',
@@ -204,3 +207,173 @@ def plotTraffic(pdTrafficRecentRelativePc, dfMedianPcSet, tsAdditionalDetail, fu
     plt.legend(loc='upper right')
     
     return [plt, fig, ax]
+
+def generateDailyProfiles(sourceData, interval = 60 * 8):
+    timezoneOffset = ((60 - np.min(sourceData.index).time().minute) * 60) if np.min(sourceData.index).time().minute > 0 else 0
+    
+    pdTrafficPreCV = sourceData[sourceData.index < dateBaselineEnd]
+    pdTrafficPostCV = sourceData[(sourceData.index >= dateBaselineEnd) & (sourceData.index < np.max(sourceData.index) - pd.Timedelta(seconds=interval))]
+    #pdTrafficPostCV
+
+    pdTrafficPreCV.insert(0, 'Day of week', pdTrafficPreCV.index.to_series().apply(lambda t: t.strftime('%A')))
+    pdTrafficPreCV.insert(0, 'Time of day', pdTrafficPreCV.index.to_series().apply(lambda t: (t if bool(t.dst()) == False else (t - pd.Timedelta(seconds=timezoneOffset))).strftime('%H:%M:%S')))
+    pdTrafficPreCVAverage = pdTrafficPreCV \
+        .groupby(['Day of week', 'Time of day'], as_index=False) \
+        .median()
+    pdTrafficPreCVLowerQuantile = pdTrafficPreCV \
+        .groupby(['Day of week', 'Time of day'], as_index=False) \
+        .quantile(0.10)
+    pdTrafficPreCVUpperQuantile = pdTrafficPreCV \
+        .groupby(['Day of week', 'Time of day'], as_index=False) \
+        .quantile(0.90)
+
+    pdTrafficPostCV.insert(0, 'Day of week', pdTrafficPostCV.index.to_series().apply(lambda t: t.strftime('%A')))
+    pdTrafficPostCV.insert(0, 'Time of day', pdTrafficPostCV.index.to_series().apply(lambda t: (t if bool(t.dst()) == False else (t - pd.Timedelta(seconds=timezoneOffset))).strftime('%H:%M:%S')))
+
+    # Give the proper order to the days in the last week
+    weekDaysOrder = list(map(lambda daysAgo: (dateToday - pd.Timedelta(days=6) + pd.Timedelta(days=daysAgo)).strftime('%A'), list(range(0, 7))))
+    pdTrafficPreCVAverage.insert(0, 'Days into last week', pdTrafficPreCVAverage['Day of week'].apply(lambda day: weekDaysOrder.index(day)))
+    pdTrafficPreCVLowerQuantile.insert(0, 'Days into last week', pdTrafficPreCVAverage['Day of week'].apply(lambda day: weekDaysOrder.index(day)))
+    pdTrafficPreCVUpperQuantile.insert(0, 'Days into last week', pdTrafficPreCVAverage['Day of week'].apply(lambda day: weekDaysOrder.index(day)))
+
+    pdTrafficPreCVAverage.sort_values(['Days into last week', 'Time of day'], inplace=True)
+    pdTrafficPreCVLowerQuantile.sort_values(['Days into last week', 'Time of day'], inplace=True)
+    pdTrafficPreCVUpperQuantile.sort_values(['Days into last week', 'Time of day'], inplace=True)
+    pdTrafficPreCVAverage = pdTrafficPreCVAverage.reset_index(drop=True)
+    pdTrafficPreCVLowerQuantile = pdTrafficPreCVLowerQuantile.reset_index(drop=True)
+    pdTrafficPreCVUpperQuantile = pdTrafficPreCVUpperQuantile.reset_index(drop=True)
+    
+    pdTrafficPreCVAverage.index = pdTrafficPreCVAverage.index.to_series().apply(
+      lambda rowId: lastWeekStart + pd.Timedelta(seconds=rowId * interval)
+    )
+    pdTrafficPreCVLowerQuantile.index = pdTrafficPreCVLowerQuantile.index.to_series().apply(
+      lambda rowId: lastWeekStart + pd.Timedelta(seconds=rowId * interval)
+    )
+    pdTrafficPreCVUpperQuantile.index = pdTrafficPreCVUpperQuantile.index.to_series().apply(
+      lambda rowId: lastWeekStart + pd.Timedelta(seconds=rowId * interval)
+    )
+    
+    baselineStart = np.min(pdTrafficPreCV.index)
+    baselineEnd = np.max(pdTrafficPreCV.index)
+    baselineLength = (baselineEnd - baselineStart).days
+    
+    return {
+        'profile': {
+            'average': pdTrafficPreCVAverage,
+            'lq': pdTrafficPreCVLowerQuantile,
+            'uq': pdTrafficPreCVUpperQuantile,
+            'length': baselineLength,
+            'start': baselineStart,
+            'end': baselineEnd
+        },
+        'recent': pdTrafficPostCV
+    }
+
+def plotDailyProfile(dp, maxChange=0.25, minBaselineDays=90, title=None):
+    fig, axs = plt.subplots(
+        4, 2,
+        figsize = (15, 6 * 4),
+        constrained_layout=True
+    )
+
+    for idx, dayOfWeek in enumerate(['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']):
+        #dayOfWeek = 'Saturday'
+
+        ax = axs[math.floor(idx / 2), idx % 2]
+
+        # Normalise the data against the normal maximum for this day of the week...
+        normalisedVolume = dp['profile']['average'][dp['profile']['average']['Day of week'] == dayOfWeek]['Total'].max(axis=0)
+
+        medianPlot = ax.plot(dp['profile']['average'][dp['profile']['average']['Day of week'] == dayOfWeek]['Total'] / normalisedVolume, label='%s average (Jul 2019 -)' % dayOfWeek, color='#233067')    
+        
+        if dp['profile']['length'] < minBaselineDays:
+            percentileLegend = None
+        else:
+            percentileLegend = ax.fill_between(
+                dp['profile']['lq'][dp['profile']['lq']['Day of week'] == dayOfWeek]['Total'].index,
+                dp['profile']['lq'][dp['profile']['lq']['Day of week'] == dayOfWeek]['Total'] / normalisedVolume,
+                dp['profile']['uq'][dp['profile']['uq']['Day of week'] == dayOfWeek]['Total'] / normalisedVolume,
+                color='#233067',
+                linewidth=0,
+                alpha=0.1,
+                label='15-85%%ile for %s' % dayOfWeek
+            )
+
+        plotWeeks = 4
+        plotAlpha = 1.0
+        plotLegends = []
+
+        for weeksAgo in range(0, plotWeeks):
+            postCVSingleWeek = dp['recent'][
+                (dp['recent'].index >= lastWeekStart - pd.Timedelta(days = 7 * weeksAgo)) &
+                (dp['recent'].index < lastWeekEnd - pd.Timedelta(days = 7 * weeksAgo))
+            ].shift(
+                periods=7 * weeksAgo,
+                freq='d'
+            )
+            
+            # Normalise and set some limits on crazy changes (usually comms outages)
+            pdWeekNormalised = postCVSingleWeek[postCVSingleWeek['Day of week'] == dayOfWeek]['Total'] / normalisedVolume
+            pdWeekNormalised = pdWeekNormalised.mask(abs(pdWeekNormalised.diff(periods=-1)) > maxChange)
+            
+            plotLegends.append(ax.plot(
+                pdWeekNormalised,
+                label=None if plotAlpha < 1.0 else '%s for last %u weeks' % (dayOfWeek, plotWeeks),
+                linestyle='solid' if plotAlpha == 1.0 else '--',
+                dashes=(2, weeksAgo * 2),
+                color='#f64a8a',
+                alpha=plotAlpha
+            )[0])
+            plotAlpha = plotAlpha - 0.25
+
+
+        timeLocatorMajor = mdates.AutoDateLocator(minticks=6, maxticks=10)
+        conciseZeroFormats = ['', '%Y', '%b', '%d-%b', '%H:%M', '%H:%M']
+        conciseOffsetFormats = ['', '%Y', '%b-%Y', '%d-%b-%Y', '%d-%b-%Y', '%d-%b-%Y %H:%M']
+        ax.xaxis.set_tick_params(which='major')
+        ax.xaxis.set_major_locator(timeLocatorMajor)
+        ax.xaxis.set_major_formatter(mdates.ConciseDateFormatter(locator=timeLocatorMajor, zero_formats=conciseZeroFormats, offset_formats=conciseOffsetFormats, tz=tzLocal))
+
+        ax.yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
+
+        ax.set_facecolor('white')
+        ax.set_clip_on(False)
+
+        ax.set_title(dayOfWeek)
+        ax.set_ylabel('Normalised vehicle counts')
+
+    # Hide the final plot...
+    ax = axs[3][1]
+    ax.axis('off')
+
+    ax.legend(
+        [
+            medianPlot[0],
+            plt.Line2D([0],[0],color="w") if percentileLegend is None else percentileLegend,
+            plt.Line2D([0],[0],color="w"),
+            plt.Line2D([0],[0],color="w")
+        ] + plotLegends,
+        [
+            'Median profile for day of week',
+            '10-90%ile for day of week' if percentileLegend is not None else 'Not enough data for quantile bounds',
+            'Baseline %s - %s' % (dp['profile']['start'].strftime('%b %Y'), dp['profile']['end'].strftime('%b %Y')),
+            ''
+        ] + [
+            'Vehicles observed in the last week',
+            '    One week ago',
+            '    Two weeks ago',
+            '    Three weeks ago'
+        ],
+        loc='upper left',
+        ncol=1
+    )
+
+    # plt.legend(ncol=1, loc='upper left', fontsize=10)
+    # plt.tight_layout()
+    
+    if title is not None:
+        fig.suptitle(title)
+        
+    plt.show()
+    
+    return fig, axs
