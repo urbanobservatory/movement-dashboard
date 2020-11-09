22bc279c75ecdf679aa9ae0d107bd7a990dbedc0
Luke Smith <luke@smith.ac>
2020-11-09 17:02:15 +0000

    Minor tweaks to wording.

--- run-nb.sh
+++ run-nb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run --rm -p 8888:8888 --user root -e JUPYTER_ENABLE_LAB=yes -e GRANT_SUDO=yes -v "/$PWD":/home/jovyan/movement-dashboard jupyter/datascience-notebook
+docker run --rm -p 18888:8888 --user root -e JUPYTER_ENABLE_LAB=yes -e GRANT_SUDO=yes -v "/$PWD":/home/jovyan/movement-dashboard jupyter/datascience-notebook
 
